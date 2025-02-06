b1fffd7a450917706aaf3d3f8d8f8d6eb66d3b02
paranoe <is-21-4-6@permaviat.ru>
2025-02-06 15:36:33 +0500

    Разработанное подключение к MySql. Дружинин

---
+++ Shop1/Data/Common/Connection.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Shop1.Data.Common
+{
+    public class Connection
+    {
+        readonly static string ConnectionData = "server=localhost;uid=root;pwd=;database=pr37;port=3306;";
+
+        public static MySqlConnection MySqlOpen()
+        {
+            MySqlConnection NewMySqlConnection = new MySqlConnection(ConnectionData);
+            NewMySqlConnection.Open();
+            return NewMySqlConnection;
+        }
+
+        public static MySqlDataReader MySqlQuery(string Query, MySqlConnection Connection)
+        {
+            MySqlCommand NewMySqlCommand = new MySqlCommand(Query, Connection);
+            return NewMySqlCommand.ExecuteReader();
+        }
+        public static void MySqlClose(MySqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
