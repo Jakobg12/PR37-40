90b17bc4e62f89766dae7edceaca5b50211253bd
paranoe <is-21-4-6@permaviat.ru>
2025-02-06 15:44:49 +0500

    Получение данных о предметах. Дружинин

---
+++ Shop1/Data/DataBase/DBItems.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using Shop1.Data.Common;
+using Shop1.Data.Interfaces;
+using Shop1.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop1.Data.DataBase
+{
+    public class DBItems : IItems
+    {
+        public IEnumerable<Categorys> Categorys = new DBCategory().AllCategorys;
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> items = new List<Items>();
+                MySqlConnection MySqlConection = Connection.MySqlOpen();
+                MySqlDataReader ItemsData = Connection.MySqlQuery("SELECT * FROM pr37-40.items ORDER BY 'Name';", MySqlConection);
+                while (ItemsData.Read())
+                {
+                    items.Add(new Items()
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? "" : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? "" : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? "" : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Category = ItemsData.IsDBNull(5) ? null : Categorys.Where(x => x.Id == ItemsData.GetInt32(5)).First()
+                    });
+                }
+                MySqlConection.Close();
+                return items;
+            }
+        }
+    }
+}
